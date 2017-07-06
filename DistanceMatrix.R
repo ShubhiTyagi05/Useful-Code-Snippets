88488e2352ea98d47c36104f8af14d7d550515c7
Shubhi Tyagi <shubhityagi05@gmail.com>
2017-07-06 09:39:22 +0530

    Add files via upload
    Get distance between text addresses in R (evading the Google 2000 hits per day solution)

---
+++ DistanceMatrix.R
@@ -0,0 +1,66 @@
+library(ggmap)
+# create vector of place names
+Sys.setlocale(category = "LC_ALL", locale = "Polish")
+options("encoding" = "native.enc")
+places_names <- read.csv("Town3.csv",sep=",",encoding="Polish",stringsAsFactors=FALSE)
+
+#  places_names<- read.csv("Town.csv",stringsAsFactors = FALSE,encoding = "UTF-8")
+places_names<-as.vector(places_names$Complete)
+
+# geocode place names
+places_lat <- geocode(places_names, source="google")$lat
+places_lon <- geocode(places_names, source="google")$lon
+
+# create a data frame to store all variables
+places_df <- data.frame(names = places_names,
+                        lat = places_lat,
+                        lon = places_lon)
+
+#Write to CSV file
+write.csv(places_df,"Lat-Lon-Address.csv")
+
+# calculate geodesic distance with gdist() from Imap package
+
+# load Imap
+#install.packages("Imap")
+library(Imap)
+
+# create an empty list
+dist_list <- list()
+
+# iterate through data frame placing calculated distance next to place place names
+for (i in 1:nrow(places_df)) {
+  
+  dist_list[[i]] <- gdist(lon.1 = places_df$lon[i], 
+                          lat.1 = places_df$lat[i], 
+                          lon.2 = places_df$lon, 
+                          lat.2 = places_df$lat, 
+                          units="km")
+  
+}
+
+# view results as list
+dist_list
+
+
+#Extracting IDs for Colnames, Rownames
+ids<- read.csv("ID.csv")
+ids<- as.vector(ids$id)
+
+# unlist results and convert to a "named" matrix format
+dist_mat <- sapply(dist_list, unlist)
+
+colnames(dist_mat) <- ids
+
+rownames(dist_mat) <- ids
+
+# view results as matrix
+dist_mat
+
+#Writing lat lon to file
+write.csv(places_df,"Lat-Lon-CompleteAddress.csv")
+write.csv(dist_mat,"Distance_Matrix-Town.csv")
+
+
+#####
+placee <- geocode("Plonsk",source = "google")
